e665be02588aa03d56938c9ff1b0f4a69385b3e9
Allison House <ajhouse05@gmail.com>
2026-05-06 21:58:33 +0200

    fix attempt 5

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const allowedOrigins = [
     'http://localhost:5173',
     'https://ahouse74.github.io',
-    'https://strayanimaltracker.com'
+    'https://capstone-server-production.up.railway.app'
 ]
 
 app.use(cors({ origin: allowedOrigins }));
